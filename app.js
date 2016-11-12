2ddc0f612edf173ce633bc7e7acd9024b483c93d
Christian Prajzner <christian@Christians-MacBook-Pro.local>
2016-11-12 13:31:45 -0500

    fixed sytax error in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-onst express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const static = express.static(__dirname + '/public');
