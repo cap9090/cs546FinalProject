cad923e6ccabfb048c26afe5d6c47ecb43ff2685
Joe <jmiles1@stevens.edu>
2016-11-23 22:29:35 -0500

    added function to find all customer finance problems

--- data/calcuations/retirement.js
+++ data/calcuations/retirement.js
@@ -1,33 +1,30 @@
 //do calculation in here and return an collection of finaical products
-const customerData = require('./customers');
-const finProdData = require('./finProds');
-const problemData = require('./problems')
+const customerData = require('../customers');
+const finProdData = require('../finProds');
+const problemData = require('../problems')
 
-module.exports = {
-calculateRetirement: (customerNodeUUID) => {
-	customerData.getCustomerByNodeUUID(customerNodeUUID).then( (customer) => {
-		customer.profile.age 
-		customer.profile.desiredRetirementAge
-		newCustomer.profile.assets.total
-		customer.profile.savingsRateOfIncome
-		customer.profile.monthlyIncome
-		annuallyCompoundedIncome()
+const inflationRate = 2.3;
+const salaryGrowthRate = 1.5
 
 
+let exportedMethods = {
+	calculateRetirement: (customerNodeUUID) => {
+		customerData.getCustomerByNodeUUID(customerNodeUUID).then( (customer) => {
+		let yearsOfRetirement = 92 - customer.profile.desiredRetirementAge;
+		let yearsUntilRetirement = customer.profile.desiredRetirementAge - customer.profile.age;
+		let fundsNeededForRetirement = .75 * yearsOfRetirement*annuallyCompoundedTotal(12*customer.profile.monthlyIncome, inflationRate + salaryGrowthRate, yearsUntilRetirement);    
+		let totalMoneySaveUpToRetirement = (customer.profile.savingsRateOfIncome/100) * totalMoneyEarnedUpToRetirement;
+		let totalLiabilities = customer.profile.liability.total;
+		let totalSavingAtRetirement = customer.profile.assets.total + totalMoneySaveUpToRetirement;
+			return totalSavingAtRetirement - totalLiabilities - fundsNeededForRetirement;
+		});
+	},
 
-
-
-
+	//total amount of money earned from now until retirement with an inflation rate of 2.3% and a salary rise of 1.5%
+	annuallyCompoundedTotal: (amount, interestRate, yearsToRetirement) => {
+		return amount*Math.pow((1 + interestRate),yearsToRetirement);
 	}
-}
+};
 
-
-
-annuallyCompoundedIncome: (monthlyIncome, inflationRate, SalaryGrowthRate, yearsCompounded) => {
-	let interestRate = inflationRate + SalaryGrowthRate;
-	return monthlyIncome * Math.pow((1 + interestRate/1), yearsCompounded);
-}
-
-
-
-}
+module.exports = exportedMethods;
+//console.log(exportedMethods.annuallyCompoundedTotal(50000, .0248, 40, 30));
