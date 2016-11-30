af57240c01dbde6a144886a9ec0faf362a5c7fa9
cbean03 <cbean03@gmail.com>
2016-11-30 15:33:46 -0500

    added tabs for financial goal options

--- public/js/customerHome.js
+++ public/js/customerHome.js
@@ -2,11 +2,23 @@
 	$('#show-update-form-button').click(() => {
 		$('#update-form').removeClass('hidden');
 		$('#show-update-form-button').addClass('hidden');
+		$('#goal-form').addClass('hidden');
+		$('#show-goal-form-button').removeClass('hidden');
 	})
 	$('#show-goal-form-button').click(() => {
 		$('#goal-form').removeClass('hidden');
 		$('#show-goal-form-button').addClass('hidden');
+		$('#update-form').addClass('hidden');
+		$('#show-update-form-button').removeClass('hidden');
+		
 	})
+	//create dropdown for retirement years
+	$(function(){
+		var $select = $(".retirementYears");
+		for (i=1;i<=50;i++){
+			$select.append($('<option></option>').val(i).html(i))
+		}
+	});
 
 
 
