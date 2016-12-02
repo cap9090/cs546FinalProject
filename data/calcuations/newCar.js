0c0b953bd3e7465c732f4c498e008df1abdc5eb7
Joe <jsmiles43@gmail.com>
2016-12-01 22:41:36 -0500

    started car calculations

--- data/calcuations/newCar.js
+++ data/calcuations/newCar.js
@@ -1,11 +1,46 @@
 //do calculation in here and return an collection of finaical products
+const calculations = require('./financialCalculations');
+const customerData = require('../customers');
+
+///*test case
+let data = {
+		monthlyPayment: 2000,
+		loanTermLength: 36,
+		financeRate: 3.3,
+		currentCarValue: 0,
+		ammountOwedOfCurrentCar: 0,
+		downPayment: 500
+};
+//*/
+
+
+exportedMethods = {
+
+	calculateCar: (userId, data)=> {
+		//customerData.getCustomerByNodeUUID(userID).then( (customer) => {
+		let loanTermLengthYears = data.loanTermLength/12;
+
+		let carCost = (() => {
+			let lumpSum = data.monthlyPayment;
+			for (let i = 1; i < data.loanTermLength; i++) {
+				lumpSum += calculations.annuallyCompoundedTotal(data.monthlyPayment, -data.financeRate, i/12);
+			}
+			return lumpSum;
+		})();
+		//let adjustedCarCost = carCost - 10*data.monthlyPayment
+
+		let costBeforeCurrentCarDedeuct = carCost + data.downPayment;
+		console.log(costBeforeCurrentCarDedeuct);
+
+		//});
+	}
+}
+
+
+module.exports = exportedMethods;
+exportedMethods.calculateCar(1, data);
 
 
 
-let monthlyPayment =
-let loanTermLength = 
-let financeRate =
 
-let currentCarValue =
-let ammountOwedOfCurrentCar = 
-let downPayment = 
+
